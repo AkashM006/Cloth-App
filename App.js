a1c3182467bd5cf856e1a3a4ca32835cb6b1783c
Akash M <akashm006@outlook.com>
2022-09-21 13:14:14 +0530

    Finished splash screen animation

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import 'react-native-reanimated'
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
-import React, { useEffect } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import React, { useEffect, useRef } from 'react'
+import { NavigationContainer, useNavigation } from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import MainScreen from './src/Screens/MainScreen'
 import { Provider } from 'react-redux'
 import {store, persistor} from './src/redux/store'
 import {Provider as PaperProvider} from 'react-native-paper'
 import { PersistGate } from 'redux-persist/integration/react'
+import Lottie from 'lottie-react-native'
 
 const SettingsScreen = () => {
   return (
@@ -25,6 +26,31 @@ const theme = {
   }
 }
 
+const SplashScreen = () => {
+  const animationRef = useRef(null);
+  const navigation = useNavigation();
+  
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Main');
+    },5000);
+  },[])
+
+  return (
+    <View style={{flex: 1, justifyContent: 'center',alignItems: 'center',}}>
+      <Lottie 
+        source={require('./src/icons/loading.json')}
+        autoPlay
+        ref={animationRef}
+        loop={false}
+        onAnimationFinish={() => {
+          animationRef.current.play()
+        }}
+      />
+    </View>
+  )
+}
+
 const App = () => {
   
   return (
@@ -38,7 +64,7 @@ const App = () => {
                       drawerActiveTintColor: 'white',
                       drawerType:'slide',
                     }}
-                    initialRouteName='Main'
+                    initialRouteName='Splash'
                   >
                       <Drawer.Screen
                         options={{
@@ -48,6 +74,16 @@ const App = () => {
                         }}
                         name='Main'
                         component={MainScreen}
+                      />
+                      <Drawer.Screen
+                        options={{
+                          header:() => {},
+                          swipeEnabled: false,
+                          drawerItemStyle: {height: 0},
+                          
+                        }}
+                        name='Splash'
+                        component={SplashScreen}
                       />
                     <Drawer.Screen name='Settings' component={SettingsScreen} />
                   </Drawer.Navigator>
