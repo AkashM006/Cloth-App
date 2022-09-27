4232675cf2a1bb59f1d650c9f0a52a0c4c89bf45
Akash M <akashm006@outlook.com>
2022-09-27 17:11:20 +0530

    Added activity indicator

--- src/components/Home/Feed.js
+++ src/components/Home/Feed.js
@@ -1,33 +1,41 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
 import React from 'react'
 import Card from './Card'
 import { FlatList } from 'react-native-gesture-handler'
 import CLOTHES from "../../data/clothes"
 import { useState } from 'react'
-import { useEffect } from 'react'
 
 const Feed = ({style}) => {
 
   const [clothes, setClothes] = useState(CLOTHES.slice(0,3));
-  const [trig,setTrig] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
+  const renderMore = () => {
+    if(isLoading === true)return;
+    setIsLoading(true);
     setTimeout(() => {
       setClothes(prevValue => [...prevValue,...prevValue.slice(0,3)])
-    },3000);
-  },[trig]);
+      setIsLoading(false);
+    },5000);
+  }
 
   return (
     <View style={[style,styles.container]}>
       <Text style={styles.title}>Autumn Winter</Text>
-      <FlatList
-        data={clothes}
-        keyExtractor={(item,index) => index}
-        renderItem={item => <Card cloth={item} />}
-        horizontal={true}
-        onEndReachedThreshold={0}
-        onEndReached={() => setTrig(t => !t)}
-      />
+      <View style={{justifyContent: 'center',alignItems:'center'}}>
+      {isLoading && <View 
+      style={styles.loader}>
+        <ActivityIndicator size='large' color='#1338BE' />
+      </View>}
+        <FlatList
+          data={clothes}
+          keyExtractor={(item,index) => index}
+          renderItem={item => <Card cloth={item} />}
+          horizontal={true}
+          onEndReachedThreshold={0}
+          onEndReached={renderMore}
+        />
+      </View>
     </View>
   )
 }
@@ -45,7 +53,21 @@ const styles = StyleSheet.create({
         fontWeight: '900',
         fontSize: 40,
         marginBottom: '15%'
+    },
+    loader: {
+      position: 'absolute', 
+      height: 50, 
+      width: 50, 
+      justifyContent: 'center',
+      alignItems: 'center',
+      zIndex: 100,
+      transform: [
+        {
+          translateY: -50,
+        }
+      ],
     }
+
 })
 
 export default Feed
