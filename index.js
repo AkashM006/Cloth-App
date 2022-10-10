f9d4c15d2181932f0b249f987b620db7d63e1938
Akash M <akashm006@outlook.com>
2022-10-10 16:53:40 +0530

    Finished firebase push notifications

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 /**
  * @format
  */
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import PushNotification from 'react-native-push-notification';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+PushNotification.configure({
+    onRegister: token => {
+        console.log("TOKEN: ", token)
+    },
+    onNotification: notification => {
+        console.log("NOTIFICATION: ", notification)
+    },
+    senderID: '909548793641'
+})
 
 AppRegistry.registerComponent(appName, () => App);
