b7b27322fdc6b56df306938133e7d1e84046756a
Akash M <akashm006@outlook.com>
2022-11-10 12:11:17 +0530

    Added full profile photo view

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet, Pressable, Platform, Alert, useWindowDimensions, TouchableOpacity } from 'react-native'
+import { View, Text, Image, StyleSheet, Pressable, Platform, Alert, useWindowDimensions, TouchableOpacity, Modal } from 'react-native'
 import React from 'react'
 import LinearGradient from 'react-native-linear-gradient'
 import { useDispatch, useSelector } from 'react-redux'
@@ -9,6 +9,7 @@ import auth from '@react-native-firebase/auth'
 import { setIsLoading, setMsg } from '../../redux/userSlice'
 import BottomSheet from '../Settings/BottomSheet'
 import { useRef } from 'react'
+import { useState } from 'react'
 
 const Profile = () => {
 
@@ -16,8 +17,61 @@ const Profile = () => {
     const dispatch = useDispatch()
     const img = user.user.photoURL === null ? require('../../icons/profile.png') : { uri: user.user.photoURL }
     const bottomSheetRef = useRef(null)
+    const [isFull, setIsFull] = useState(false)
 
     const { height, window } = useWindowDimensions()
+
+    const photoSelectionCallback = async response => {
+        if (response.didCancel) {
+            console.log("User cancelled file upload")
+            dispatch(setIsLoading(false))
+            return
+        }
+        if (response.errorCode) {
+            dispatch(setIsLoading(false))
+            console.log("Error message: ", response.errorMessage)
+            // Alert.alert('Whoops!', response.errorMessage, [{ text: 'OK' }], { cancelable: true })
+            dispatch(setMsg({ title: 'Whoops!', text: response.errorMessage, status: 'failure' }))
+            return
+        }
+
+        let imgUri = response.assets[0].uri
+        imgUri = Platform.OS === 'android' ? imgUri.replace('file://', '') : imgUri
+        let fileName = imgUri.substring(imgUri.lastIndexOf('/') + 1)
+
+
+        try {
+            await storage().ref(fileName).putFile(imgUri)
+        } catch (err) {
+            console.log("Unable to upload to firebase storage: ", err)
+            dispatch(setIsLoading(false))
+            return
+        }
+
+        let fileUrl = ''
+        try {
+            fileUrl = await storage().ref(fileName).getDownloadURL()
+        } catch (err) {
+            console.log("Unable to get file: ", err)
+            dispatch(setIsLoading(false))
+            return
+        }
+
+        const update = { photoURL: fileUrl }
+
+        try {
+            await auth().currentUser.updateProfile(update)
+        } catch (err) {
+            dispatch(setIsLoading(false))
+            console.log("Unable to change the profile photo: ", err)
+            return
+        }
+
+        dispatch(setIsLoading(false))
+        // Alert.alert('Hooray!', 'Your profile photo has changed!', [{ text: 'OK' }], { cancelable: true })
+        dispatch(setMsg({ title: 'Hooray!', text: 'Your profile photo has been changed!', status: 'success' }))
+        bottomSheetRef.current?.scrollTo(0)
+    }
 
     const cameraHandler = () => {
         dispatch(setIsLoading(true))
@@ -31,57 +85,7 @@ const Profile = () => {
             }
         }
 
-        launchCamera(options, async response => {
-            if (response.didCancel) {
-                console.log("User cancelled file upload")
-                dispatch(setIsLoading(false))
-                return
-            }
-            if (response.errorCode) {
-                dispatch(setIsLoading(false))
-                console.log("Error message: ", response.errorMessage)
-                // Alert.alert('Whoops!', response.errorMessage, [{ text: 'OK' }], { cancelable: true })
-                dispatch(setMsg({ title: 'Whoops!', text: response.errorMessage, status: 'failure' }))
-                return
-            }
-
-            let imgUri = response.assets[0].uri
-            imgUri = Platform.OS === 'android' ? imgUri.replace('file://', '') : imgUri
-            let fileName = imgUri.substring(imgUri.lastIndexOf('/') + 1)
-
-
-            try {
-                await storage().ref(fileName).putFile(imgUri)
-            } catch (err) {
-                console.log("Unable to upload to firebase storage: ", err)
-                dispatch(setIsLoading(false))
-                return
-            }
-
-            let fileUrl = ''
-            try {
-                fileUrl = await storage().ref(fileName).getDownloadURL()
-            } catch (err) {
-                console.log("Unable to get file: ", err)
-                dispatch(setIsLoading(false))
-                return
-            }
-
-            const update = { photoURL: fileUrl }
-
-            try {
-                await auth().currentUser.updateProfile(update)
-            } catch (err) {
-                dispatch(setIsLoading(false))
-                console.log("Unable to change the profile photo: ", err)
-                return
-            }
-
-            dispatch(setIsLoading(false))
-            // Alert.alert('Hooray!', 'Your profile photo has changed!', [{ text: 'OK' }], { cancelable: true })
-            dispatch(setMsg({ title: 'Hooray!', text: 'Your profile photo has been changed!', status: 'success' }))
-            bottomSheetRef.current?.scrollTo(0)
-        })
+        launchCamera(options, photoSelectionCallback)
     }
 
     const galleryHandler = () => {
@@ -94,72 +98,39 @@ const Profile = () => {
             selectionLimit: 1,
         }
 
-        launchImageLibrary(options, async response => {
-            if (response.didCancel) {
-                console.log("User cancelled file upload")
-                dispatch(setIsLoading(false))
-                return
-            }
-            if (response.errorCode) {
-                dispatch(setIsLoading(false))
-                console.log("Error message: ", response.errorMessage)
-                // Alert.alert('Whoops!', response.errorMessage, [{ text: 'OK' }], { cancelable: true })
-                dispatch(setMsg({ title: 'Whoops!', text: response.errorMessage, status: 'failure' }))
-                return
-            }
-
-            let imgUri = response.assets[0].uri
-            imgUri = Platform.OS === 'android' ? imgUri.replace('file://', '') : imgUri
-            let fileName = imgUri.substring(imgUri.lastIndexOf('/') + 1)
-
-            try {
-                await storage().ref(fileName).putFile(imgUri)
-            } catch (err) {
-                console.log("Unable to upload to firebase storage: ", err)
-                dispatch(setIsLoading(false))
-                return
-            }
-
-            let fileUrl = ''
-            try {
-                fileUrl = await storage().ref(fileName).getDownloadURL()
-            } catch (err) {
-                console.log("Unable to get file: ", err)
-                dispatch(setIsLoading(false))
-                return
-            }
-
-            const update = { photoURL: fileUrl }
-
-            try {
-                await auth().currentUser.updateProfile(update)
-            } catch (err) {
-                dispatch(setIsLoading(false))
-                console.log("Unable to change the profile photo: ", err)
-                return
-            }
-
-            dispatch(setIsLoading(false))
-            // Alert.alert('Hooray!', 'Your profile photo has changed!', [{ text: 'OK' }], { cancelable: true })
-            dispatch(setMsg({ title: 'Hooray!', text: 'Your profile photo has been changed!', status: 'success' }))
-            bottomSheetRef.current?.scrollTo(0)
-        })
+        launchImageLibrary(options, photoSelectionCallback)
     }
 
-    const pressHandler = () => {
-        bottomSheetRef.current?.scrollTo(-height / 2.25)
-
-    }
+    const pressHandler = () => { bottomSheetRef.current?.scrollTo(-height / 2.25) }
 
     return (
         <View style={{ flex: 1, backgroundColor: 'white', }}>
+            <Modal
+                transparent={true}
+                animationType='slide'
+                visible={isFull}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalHeader}>
+                        <Pressable onPress={() => setIsFull(false)}>
+                            <Image source={require('../../icons/close.png')} style={styles.close} />
+                        </Pressable>
+                        <Text style={styles.modalText}>{user.user.displayName}</Text>
+                    </View>
+                    <View style={styles.modalImage}>
+                        <Image source={img} style={styles.photoFull} />
+                    </View>
+                </View>
+            </Modal>
             <View style={{ flex: 1, }} />
             <View style={{ flex: 5, backgroundColor: 'white', }}>
                 <LinearGradient colors={['#DCE2E2', 'rgba(217, 217, 217, 0)']} locations={[0, 1]} style={styles.gradientContainer}>
                     <View style={{ transform: [{ translateY: -90 }], width: '100%', }}>
                         <Text style={styles.title}>{user.user.displayName}</Text>
                         <View>
-                            <Image source={img} style={styles.photo} />
+                            <Pressable onPress={() => setIsFull(true)}>
+                                <Image source={img} style={styles.photo} />
+                            </Pressable>
                             <Pressable onPress={pressHandler} style={styles.cameraContainer}>
                                 <Image source={require('../../icons/pencil.png')} style={styles.camera} />
                             </Pressable>
@@ -170,7 +141,7 @@ const Profile = () => {
                 <BottomSheet ref={bottomSheetRef}>
                     <View style={styles.bottomSheetContainer}>
                         <TouchableOpacity onPress={galleryHandler}>
-                            <View style={[styles.iconContainer, { marginRight: '5%' }]}>
+                            <View style={styles.iconContainer}>
                                 <Image style={styles.icon} source={require('../../icons/gallery.png')} />
                                 <Text style={styles.text}>Select from Gallery</Text>
                             </View>
@@ -235,7 +206,6 @@ const styles = StyleSheet.create({
     bottomSheetContainer: {
         flexDirection: 'row',
         paddingHorizontal: '5%',
-        // marginHorizontal: '5%',
         justifyContent: 'center',
     },
     iconContainer: {
@@ -243,6 +213,7 @@ const styles = StyleSheet.create({
         padding: '2.5%',
         alignItems: 'center',
         borderRadius: 10,
+        marginRight: '5%',
     },
     text: {
         color: 'white',
@@ -251,6 +222,34 @@ const styles = StyleSheet.create({
     },
     icon: {
         tintColor: 'white'
+    },
+    photoFull: {
+        height: '80%',
+        width: '100%'
+    },
+    modalContainer: {
+        backgroundColor: 'black',
+        paddingVertical: '5%'
+    },
+    modalHeader: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    close: {
+        width: 30,
+        height: 30,
+        tintColor: 'white'
+    },
+    modalText: {
+        color: 'white',
+        marginLeft: '2%',
+        fontSize: 20,
+        fontWeight: '700'
+    },
+    modalImage: {
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%',
     }
 })
 
